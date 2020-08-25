3892a3c625035e1fe726646dbcf0cea3c95d7263
ibrahemig18 <48921347+ibrahemig18@users.noreply.github.com>
2020-08-25 13:04:23 +0300

    Spring boot
    Spring boot

---
+++ OnTheGoShop/OnTheGoShop/src/main/java/com/project/OnTheGoShop/OnTheGoShopApplication.java
@@ -0,0 +1,13 @@
+package com.project.OnTheGoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnTheGoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnTheGoShopApplication.class, args);
+	}
+
+}
---
+++ OnTheGoShop/OnTheGoShop/src/test/java/com/project/OnTheGoShop/OnTheGoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.OnTheGoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnTheGoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
