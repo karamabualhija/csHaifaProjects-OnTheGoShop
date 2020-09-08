69439b9b392b033bacdbb432aacb87fc283bb45c
ibrahemig18 <48921347+ibrahemig18@users.noreply.github.com>
2020-09-08 15:53:02 +0300

    android
    ..

---
+++ OnTheGoShopClient/app/src/main/java/com/tsofen/onthegoshopClient/register.java
@@ -0,0 +1,18 @@
+package com.tsofen.onthegoshopClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void register(View view) {
+    }
+}
